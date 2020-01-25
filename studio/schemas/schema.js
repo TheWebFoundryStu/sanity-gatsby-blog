7b62f587d7eeeb97890bfb0916a12928b6355836
TheWebFoundryStu <stuart.moran@thewebfoundry.net>
2020-01-25 22:56:04 +0000

    P

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -8,6 +8,7 @@ import schemaTypes from 'all:part:@sanity/base/schema-type'
 import author from './documents/author'
 import category from './documents/category'
 import post from './documents/post'
+import page from './documents/page'
 import siteSettings from './documents/siteSettings'
 
 // Object types
