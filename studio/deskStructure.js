c5d5bad502b21a428fe7c6e4322bca0c43af42e4
TheWebFoundryStu <stuart.moran@thewebfoundry.net>
2020-01-25 22:29:19 +0000

    Add Page to Studio add list

--- studio/deskStructure.js
+++ studio/deskStructure.js
@@ -3,36 +3,40 @@ import MdSettings from 'react-icons/lib/md/settings'
 import MdPerson from 'react-icons/lib/md/person'
 
 const hiddenDocTypes = listItem =>
-  !['category', 'author', 'post', 'siteSettings'].includes(listItem.getId())
+    !['category', 'author', 'post', 'siteSettings'].includes(listItem.getId())
 
 export default () =>
-  S.list()
+S.list()
     .title('Content')
     .items([
-      S.listItem()
+        S.listItem()
         .title('Settings')
         .icon(MdSettings)
         .child(
-          S.editor()
+            S.editor()
             .id('siteSettings')
             .schemaType('siteSettings')
             .documentId('siteSettings')
         ),
-      S.listItem()
+        S.listItem()
         .title('Blog posts')
         .schemaType('post')
         .child(S.documentTypeList('post').title('Blog posts')),
-      S.listItem()
+        S.listItem()
+        .title('Pages')
+        .schemaType('page')
+        .child(S.documentTypeList('post').title('Pages')),
+        S.listItem()
         .title('Authors')
         .icon(MdPerson)
         .schemaType('author')
         .child(S.documentTypeList('author').title('Authors')),
-      S.listItem()
+        S.listItem()
         .title('Categories')
         .schemaType('category')
         .child(S.documentTypeList('category').title('Categories')),
-      // This returns an array of all the document types
-      // defined in schema.js. We filter out those that we have
-      // defined the structure above
-      ...S.documentTypeListItems().filter(hiddenDocTypes)
+        // This returns an array of all the document types
+        // defined in schema.js. We filter out those that we have
+        // defined the structure above
+        ...S.documentTypeListItems().filter(hiddenDocTypes)
     ])
